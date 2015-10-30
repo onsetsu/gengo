51f36f6968247fad22288553ac796ebd9926607f
Stefan Lehmann <stefan.lehmann@student.hpi.uni-potsdam.de>
2015-10-30 10:13:15 +0100

    css class dragged

--- visual.js
+++ visual.js
@@ -32,31 +32,64 @@ var data = [
     }
 ];
 
-var drag = d3.behavior.drag()
-    .origin(function(d) { return d; })
-    .on("drag", dragmove);
-
 var svg = d3.select("body").append("svg")
     .attr("width", width)
     .attr("height", height);
 
-var newg = svg.selectAll("g")
+// dragging
+var drag = d3.behavior.drag()
+    .origin(function(d) { return d; })
+    .on("dragstart", dragstarted)
+    .on("drag", dragged)
+    .on("dragend", dragended);
+
+function dragstarted(d) {
+    d3.event.sourceEvent.stopPropagation();
+    d3.select(this).classed("dragging", true);
+}
+
+function dragged(d) {
+    d3.select(this)
+        .attr("transform", function(d) {
+            return 'translate('+ (d.x = d3.event.x)+', '+ (d.y = d3.event.y)+')';
+        });
+}
+
+function dragended(d) {
+    d3.select(this).classed("dragging", false);
+}
+
+// zooming
+var zoomBehavior = d3.behavior.zoom()
+    //.x(x)
+    //.y(y)
+    .scaleExtent([1, 8])
+    .on("zoom", zoom);
+
+
+function zoomed() {
+    container.attr("transform", "translate(" + d3.event.translate + ")scale(" + d3.event.scale + ")");
+}
+
+function zoom() {
+    zoomGroup.attr("transform", "translate(" + d3.event.translate + ")scale(" + d3.event.scale + ")");
+}
+var zoomGroup = svg.append('g')
+    .call(zoomBehavior)
+    .append('g');
+
+var nodeGroup = zoomGroup.selectAll("g")
     .data(data)
     .enter().append("g")
     .attr("transform", function(d) { return 'translate('+d.x+', '+d.y+')'; })
     .attr("cursor", "move")
     .call(drag);
 
-var dragrect = newg.append("rect")
+nodeGroup.append("rect")
     .attr("width", function(d) { return d.width; })
     .attr("height", function(d) { return d.height; })
     .attr("fill-opacity", .5);
 
-newg.append("text")
-    //.attr("dy", ".35em")
+nodeGroup.append("text")
     .text(function(d) { return d.name; });
 
-function dragmove(d) {
-    d3.select(this)
-        .attr("transform", function(d) { return 'translate('+ (d.x = d3.event.x)+', '+ (d.y = d3.event.y)+')'; })
-}
